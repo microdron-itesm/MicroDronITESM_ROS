82e28c864ea18460a6b3c7251fcf7158ee421525
Abiel <empoleom@gmail.com>
2020-09-01 11:33:21 -0500

    Use UDPLib

--- src/itesm_simulator/mavlink_imu_interface/src/mavlink_imu_interface_node.cpp
+++ src/itesm_simulator/mavlink_imu_interface/src/mavlink_imu_interface_node.cpp
@@ -11,6 +11,10 @@
 #include <arpa/inet.h>
 #include <thread>
 
+extern "C" {
+#include <UDP.h>
+}
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "mavlink_imu");
     ros::NodeHandle nodeHandle;
@@ -21,42 +25,28 @@ int main(int argc, char **argv){
         ROS_WARN("mavlink_imu was not given a mav_name! Using \"ardrone\" as default");
     }
 
-    struct sockaddr_in localAddress{};
-    const uint16_t imu_send_port = 14551;
-    int sockFd; // Socket file descriptor
-
-    sockFd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if(sockFd < 0){
-        perror("Socket open failed");
-        exit(EXIT_FAILURE);
-    }
-
-    memset(&localAddress, 0, sizeof(localAddress));
-    localAddress.sin_family = AF_INET;
-    localAddress.sin_addr.s_addr = INADDR_ANY;
-    localAddress.sin_port = htons(imu_send_port);
-
-    int ret = fcntl(sockFd, F_SETFL, O_NONBLOCK | O_ASYNC);
-    if(ret < 0){
-        perror("Nonblocking set failed");
-        close(sockFd);
-        exit(EXIT_FAILURE);
-    }
-
     size_t bufLen = MAVLINK_MAX_PACKET_LEN + sizeof(uint64_t);
     uint8_t buf[bufLen];
 
+    udp_conn_data data;
+    udp_conn_open_ip(&data, "localhost", 14552, 14553);
+
     mavlink_attitude_quaternion_t attitude;
     MAVLinkIMUInterface imuInterface(droneName);
+
+    ros::Rate rate(60);
     while(ros::ok()){
         memset(&buf, 0, sizeof(buf));
         attitude = imuInterface.getAttitude();
         mavlink_message_t msg;
         mavlink_msg_attitude_quaternion_pack(1, 200, &msg, attitude.time_boot_ms, attitude.q1, attitude.q2, attitude.q3, attitude.q4, attitude.rollspeed, attitude.pitchspeed, attitude.yawspeed, attitude.repr_offset_q);
         int len = mavlink_msg_to_send_buffer(buf, &msg);
-        sendto(sockFd, buf, len, 0, (struct sockaddr*) &localAddress, sizeof(struct sockaddr_in));
+        udp_conn_send(&data, buf, len);
         ros::spinOnce();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        rate.sleep();
     }
+
+    udp_conn_close(&data);
+
     return 1;
 }
