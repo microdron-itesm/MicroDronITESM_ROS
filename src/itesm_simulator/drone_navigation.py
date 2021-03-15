3d7f81992bee14c8cd2b9265b18891e64ac34d1f
Jesus Omar <jesomar.cuenca@gmail.com>
2021-03-15 14:39:34 -0600

    Functioning 2 drone Pursuing Dance

--- src/itesm_simulator/drone_navigation.py
+++ src/itesm_simulator/drone_navigation.py
@@ -7,19 +7,13 @@ from tf.transformations import quaternion_from_euler
 import time
 import math
 
-z = .5
-
-class Commander:
-    def __init__(self,topic):
-        rospy.init_node("commander_node")
-        rate = rospy.Rate(20)
-        self.current_x = 0
-        self.current_y = 0
-        self.current_z = .5
+class Follower:
+    def __init__(self,x,y,z,topic):
+        self.current_x = x
+        self.current_y = y
+        self.current_z = z
         self.current_yaw = quaternion_from_euler(0, 0, 0)
         self.position_target_pub = rospy.Publisher(f'/{topic}/command/pose', PoseStamped, queue_size=10)
-        self.position_target_pub.publish(self.set_pose(self.current_x , self.current_y, self.current_z, True))
-
 
     def move(self, x, y, z, BODY_OFFSET_ENU=True):
         self.position_target_pub.publish(self.set_pose(x, y, z, BODY_OFFSET_ENU))
@@ -32,13 +26,14 @@ class Commander:
         self.position_target_pub.publish(self.set_pose(0, 0, height, False))
 
 
-    def set_pose(self, x=0, y=0, z=2, BODY_FLU = True):
+    def set_pose(self, x=0, y=0, z=2, bodyFlu = True):
         pose = PoseStamped()
         pose.header.stamp = rospy.Time.now()
 
         # ROS uses ENU internally, so we will stick to this convention
-        if BODY_FLU:
-            pose.header.frame_id = 'base_link'
+        if bodyFlu:
+            # pose.header.frame_id = 'base_link'
+            pose.header.frame_id = 'auto'
 
         else:
             pose.header.frame_id = 'map'
@@ -46,7 +41,7 @@ class Commander:
         pose.pose.position.x = x
         pose.pose.position.y = y
         pose.pose.position.z = z
-        pose.pose.orientation = Quaternion(*self.current_yaw)
+        # pose.pose.orientation = Quaternion(*self.current_yaw)
 
         self.current_x = x
         self.current_y = y
@@ -55,46 +50,90 @@ class Commander:
 
         return pose
 
-    def set_orientation(self, yaw):
-        pose = PoseStamped()
-        pose.header.stamp = rospy.Time.now()
-        if BODY_FLU:
-            pose.header.frame_id = 'base_link'
+    # def set_orientation(self, yaw):
+    #     pose = PoseStamped()
+    #     pose.header.stamp = rospy.Time.now()
+    #     if BODY_FLU:
+    #         pose.header.frame_id = 'base_link'
 
-        else:
-            pose.header.frame_id = 'map'
+    #     else:
+    #         pose.header.frame_id = 'map'
 
 
-        DEG_2_RAD = math.pi / 180.0
-        pose.pose.position.x = self.current_x
-        pose.pose.position.y = self.current_y
-        pose.pose.position.z = self.current_z
+    #     DEG_2_RAD = math.pi / 180.0
+    #     pose.pose.position.x = self.current_x
+    #     pose.pose.position.y = self.current_y
+    #     pose.pose.position.z = self.current_z
 
         
-        q = quaternion_from_euler(yaw*DEG_2_RAD, 0, yaw*DEG_2_RAD)
-        print(Quaternion(*q))
-        pose.pose.orientation = Quaternion(*q)
+    #     q = quaternion_from_euler(yaw*DEG_2_RAD, 0, yaw*DEG_2_RAD)
+    #     print(Quaternion(*q))
+    #     pose.pose.orientation = Quaternion(*q)
 
-        self.current_yaw = q
+    #     self.current_yaw = q
 
-        return pose
+    #     return pose
+
+class Commander:
+
+    AVAILABLE_DRONES    = ("gs0","gs1")
+    INITIAL_POSITIONS   = (
+        (0,0,0),
+        (4,0,0)
+    )
+    CONSTANT_Z = .8
+
+    def __init__(self,instructions : list,looping = False):
+        rospy.init_node("commander_node")
+        rate = rospy.Rate(20)
+        self.wayPoints = instructions
+        self.loopDrones = looping
+        self.numDrones = len(instructions)
+        self.drones = []
+        for x in range(self.numDrones):
+            t = Commander.INITIAL_POSITIONS[x]
+            self.drones.append(Follower(t[0],t[1],t[2],Commander.AVAILABLE_DRONES[x]))
+        self.currentWaypoint = 0
+
+    def nextPosition(self) -> int:
+        if(self.currentWaypoint >= len(self.wayPoints[0])):
+            return -1
+        i = 0
+        for d in self.drones:
+            waypoints = self.wayPoints[i][self.currentWaypoint]
+            # d.move(waypoints[0],waypoints[1],waypoints[2])
+            d.move(waypoints[0],waypoints[1],Commander.CONSTANT_Z)
+            i += 1
+        self.currentWaypoint += 1
+        if(self.currentWaypoint >= len(self.wayPoints[0]) and self.loopDrones):
+            self.currentWaypoint = 0
+        return self.currentWaypoint
+
+
+def readInstructions() -> list:
+    f = open("visualize.csv","r")
+    instructions = f.readlines()
+    f.close()
+    result = []
+    for x in instructions:
+        s = x.strip().split(",")
+        temp = []
+        for y in s:
+            tempFactors = y.split(";")
+            if(len(tempFactors) > 1):
+                temp.append(tuple([float(z) for z in tempFactors]))
+        result.append(tuple(temp))
+    return result
 
 if __name__ == "__main__":
-    
-    delay = 7
-    extension = 2
-    con1 = Commander("gs0")
-    con2 = Commander("gs1")
-    time.sleep(2)
-    con1.move(1, 1, 1)
-    time.sleep(2)
-    con2.move(-1,-2.5,.5)
-    time.sleep(5)
-    con2.move(0,-.5,.5)
-    # time.sleep(delay)
-    # con.move(-extension, 0, z)
-    # time.sleep(delay)
-    # con.move(0, 0, z)
-    # time.sleep(delay)
-    # con.move(0, -extension, z)
 
+    instructions = readInstructions()
+
+    c = Commander(instructions)
+    time.sleep(1)
+    try:
+        while(c.nextPosition() >= 0):
+                time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    print("Recorrido concluido")
