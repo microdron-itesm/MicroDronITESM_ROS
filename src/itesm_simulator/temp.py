78103f583b0db529628d0597d32975ee2b580c61
Jesus Omar <jesomar.cuenca@gmail.com>
2021-05-24 21:09:45 -0500

    Preparing for integration into the drones

--- src/itesm_simulator/temp.py
+++ src/itesm_simulator/temp.py
@@ -11,26 +11,15 @@ def measureTime(fun):
         return result
     return measureExTime
 
-seed(91939)
-
-nextPositions = [
-    [0.0 ,  1.6,    2.5],
-    [0.0 ,  0.8,    1.5],
-    [0   ,  0,      2.5],
-    [-0.0,  -0.8,   1.5],
-    [-0.0,  -1.6,   2.5],
-    [0.0 ,  1.6,    2.0],
-    [0.0 ,  0.8,    1.0],
-    [0   ,  0,      2.0]
-]
-
-def genRandomCoords():
+def genRandomCoords(coordLen, replicable = False):
+    if replicable :
+        seed(91939)
     floatFactor = .1
     return [[
         -round2f(randint(0,25)*floatFactor) if randint(0,100) % 2 == 0 else round2f(randint(0,25)*floatFactor),
         -round2f(randint(0,25)*floatFactor) if randint(0,100) % 2 == 0 else round2f(randint(0,25)*floatFactor),
         -round2f(randint(0,25)*floatFactor) if randint(0,100) % 2 == 0 else round2f(randint(0,25)*floatFactor)
-    ] for x in range(len(nextPositions))]
+    ] for x in range(coordLen)]
 
 def calculate_distances(position, otherPositions):
     return [round2f(euclidianDistance(
@@ -64,18 +53,20 @@ def aStar(curr : list, distances : list, memory : dict):
             temp.append(position)
             allDistances.append(aStar(temp, distances, memory))
     return min(allDistances)
-    
 
-def path_plan():
-    current_positions = genRandomCoords()
+def path_plan(current_positions, next_positions, verbose = False):
     distances = [calculate_distances(currP,nextPositions) for currP in current_positions]
+    if verbose :
+        for position in range(len(current_positions)):
+            print(f"For position {current_positions[position]}\t distances are\t {distances[position]}")
     hashMemory = {}
     result = aStar([], distances, hashMemory)
     totalPathCombinations = {}
     for key in hashMemory.keys():
         partsOfKey = key.split(',')
         if(len(partsOfKey) == len(distances)):
-            print(f"Key {key}\tequals a distance of\t{hashMemory[key]}")
+            if verbose:
+                print(f"Key {key}\tequals a distance of\t{hashMemory[key]}")
             totalPathCombinations[hashMemory[key]] = partsOfKey
 
     return totalPathCombinations[result], result
@@ -86,7 +77,18 @@ def factorial(n):
     return n*factorial(n-1)
 
 if __name__ == "__main__":
+    nextPositions = [
+        [0.0 ,  1.6,    2.5],
+        [0.0 ,  0.8,    1.5],
+        [0   ,  0,      2.5],
+        [-0.0,  -0.8,   1.5],
+        [-0.0,  -1.6,   2.5],
+        # [0.0 ,  1.6,    2.0],
+        # [0.0 ,  0.8,    1.0],
+        # [0   ,  0,      2.0]
+    ]
     meassured = measureTime(path_plan)
-    combination, minDistance = meassured()
+    v = input("Verbose? (y)")
+    combination, minDistance = meassured(genRandomCoords(len(nextPositions)), nextPositions, v == 'y')
     print(f"The best combination being {combination}\ttraveling a distance of {minDistance}")
     # print(factorial(100))
