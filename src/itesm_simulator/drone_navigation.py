2ed7a0bb4224c54ba6f74ebd2107acde81902428
hectornleon <hectornleon98@gmail.com>
2021-05-29 14:00:11 -0700

    Added collision avoidance prototype

--- src/itesm_simulator/drone_navigation.py
+++ src/itesm_simulator/drone_navigation.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 import rospy
-from typing import List
-from geometry_msgs.msg import PoseStamped, PointStamped
+from typing import Dict, List
+from geometry_msgs.msg import PoseStamped, PointStamped,Transform, Quaternion, Point, Twist
+from trajectory_msgs.msg import MultiDOFJointTrajectory, MultiDOFJointTrajectoryPoint
 from sensor_msgs.msg import Imu, NavSatFix
 from std_msgs.msg import Float32, String
 from pyquaternion import Quaternion
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, vector_norm
 from readers    import *
 from enums      import *
 from utils.utils      import *
 import time
 import math
+import numpy as np
+
+positions = dict()
 
 class Follower:
     def __init__(self,topic : str,heightFactor = 1):
@@ -20,23 +24,55 @@ class Follower:
         self.desired_x = .0
         self.desired_y = .0
         self.desired_z = .0
+        self.stored = dict()
+        self.observing = []
+        self.observed = False
         self.id = topic
         self.heightFactor = heightFactor
         self.current_yaw = quaternion_from_euler(0, 0, 0)
         self.position_target_pub = rospy.Publisher(f'/{topic}/command/pose', PoseStamped, queue_size=10)
+
+        ## TODO MAKE PUBLISHER STOP WHEN COLLISION
         rospy.Subscriber(f"/{topic}/odometry_sensor1/position", PointStamped, self.position_callback)
 
     def position_callback(self, data):
+        global positions
         self.current_x = round2f(data.point.x)
         self.current_y = round2f(data.point.y)
         self.current_z = round2f(data.point.z)
+        positions[self.id] = [self.current_x,self.current_y,self.current_z]
+        for d in self.observing:
+            if(d in positions):
+                distance = euclidianDistance(
+                    self.current_x, positions[d][0],
+                    self.current_y, positions[d][1], 
+                    self.current_z, positions[d][2]
+                )
+                if(distance > 0.5 and self.stored.get(d) != None and len(self.stored.keys()) == 1):
+                    print(distance,self.id,d, 'reseting')
+                    self.move(self.stored[d][0],self.stored[d][1],self.stored[d][2])
+                    self.observing.remove(d)
+                    del self.stored[d]
+                    print(self.stored)
+                elif(distance < 1.0 and (self.stored.get(d) == None)):
+                    print(distance,self.id,d, 'less')
+                    self.stored[d] = [self.desired_x,self.desired_y,self.desired_z]
+                    self.move(positions[d][0]-positions[d][0],positions[d][1]+positions[d][1],self.current_z)
+                elif(distance > 1. and self.stored.get(d) != None):
+                    print(distance,self.id,d, 'reseting')
+                    self.move(self.stored[d][0],self.stored[d][1],self.stored[d][2])
+                    self.observing.remove(d)
+                    del self.stored[d]
+                    print(self.stored)
 
     def ready(self) -> bool:
-        return euclidianDistance(
-            self.current_x, self.desired_x,
-            self.current_y, self.desired_y,
-            self.current_z, self.desired_z
-        ) < .1
+        if(self.stored.get(self.id) == None):
+            return euclidianDistance(
+                self.current_x, self.desired_x,
+                self.current_y, self.desired_y,
+                self.current_z, self.desired_z
+            ) < .4
+        return False
 
     def move(self, x, y, z, BODY_OFFSET_ENU=True):
         self.position_target_pub.publish(self.set_pose(x, y, z, BODY_OFFSET_ENU))
@@ -49,11 +85,15 @@ class Follower:
         self.position_target_pub.publish(self.set_pose(0, 0, height, False))
 
     def get_location(self) -> dict:
-        return {
-            "x" : self.current_x,
-            "y" : self.current_y,
-            "z" : self.current_z
-        }
+        return [
+            self.current_x,
+            self.current_y,
+            self.current_z
+        ]
+    
+    def setObservations(self, obsverving, obsvered=False):
+        self.observing = self.observing + obsverving
+        if(not self.observed): self.observed = obsvered
 
 
     def set_pose(self, x=0, y=0, z=2, bodyFlu = True):
@@ -102,6 +142,7 @@ class Commander:
         if(self.currentWaypoint >= len(self.wayPoints[0])):
             return -1
         i = 0
+        self.calculateCollision()
         for d in self.drones:
             if(d.id == Commander.AVAILABLE_DRONES[0]):
                 d.get_location()
@@ -125,6 +166,31 @@ class Commander:
                 return True
         return False
 
+    def calculateD(self, m, n, o, p, Points):
+        return (Points[m][0] - Points[n][0])*(Points[o][0] - Points[p][0])+ \
+               (Points[m][1] - Points[n][1])*(Points[o][1] - Points[p][1])+ \
+               (Points[m][2] - Points[n][2])*(Points[o][2] - Points[p][2])
+
+    def calculateCollision(self):
+        points =  self.getAllPositions()
+        for p in range(len(points)):
+            points[p] = [points[p], self.wayPoints[p][self.currentWaypoint]]
+        for i in range(len(self.drones)):
+            dronePoints = [points[i][0], points[i][1]]
+            for j in range(i+1,len(self.drones)):
+                usePoints = dronePoints + [points[j][0], points[j][1]]
+                mua = (self.calculateD(0,2,3,2,usePoints)*self.calculateD(3,2,1,0,usePoints) - self.calculateD(0,2,1,0,usePoints)*self.calculateD(3,2,3,2,usePoints)) / \
+                    (self.calculateD(1,0,1,0,usePoints)*self.calculateD(3,2,3,2,usePoints) - self.calculateD(3,2,1,0,usePoints)*self.calculateD(3,2,1,0,usePoints))
+                mub = (self.calculateD(0,2,3,2,usePoints)+mua*self.calculateD(3,2,1,0,usePoints)) / self.calculateD(3,2,3,2,usePoints)
+                Pa = np.add(usePoints[0], mua*(np.subtract(usePoints[0],usePoints[1])))
+                Pb = np.add(usePoints[2], mub*(np.subtract(usePoints[3],usePoints[2])))
+                distance = self.ecluedianNorm(np.subtract(Pb,Pa))**2
+                if(distance < 15.0):
+                    self.drones[i].setObservations([Commander.AVAILABLE_DRONES[j]])
+                    self.drones[j].setObservations([], True)
+    
+    def ecluedianNorm(self, p):
+        return math.sqrt((p[0]**2+p[1]**2+p[2]**2))
 
 if __name__ == "__main__":
 
